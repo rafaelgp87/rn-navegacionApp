e370ac34bca7f8743445a06e570a946c5816a748
Rafael Gutierrez <rafaelgp87@gmail.com>
2021-07-25 14:31:58 -0500

    Primer commit.

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView
+      style= {{ flex: 1 }}
+    >
+      <View>
+        <Text>App Screen</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default App;
